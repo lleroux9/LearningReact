eb249c78e113e4c08c672c3ed0549afac8c367b9
lleroux9 <loganleroux9@gmail.com>
2026-03-26 21:46:46 -0400

    Added my first react element and started messing around with state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import './App.css';
+import Employee from './components/Employee';
+import { useState } from 'react';
+
 
 function App() {
+  const [role, setRole] = useState('dev');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-          This is Logan Leroux attempting to learn web app development
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ? (
+          <>
+            <input
+              type="text"
+              onChange={(e) => {
+                console.log(e.target.value);
+                setRole(e.target.value)
+              }}
+            />
+            <Employee name='Logan' age='21' role='Mechatronic Systems Engineer'/>
+            <Employee name='Liam' role='CPA'/>
+            <Employee name='Lucas' age='22' role={role}/>
+            <Employee name='Caiden'/>
+            <Employee name='Ben' age='22'/>   
+
+          </>
+      ) : (
+        <p>You cannot see employees</p>
+          )
+      }
+
     </div>
   );
 }
+
 
 export default App;
